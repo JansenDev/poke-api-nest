fab9701171033e6169f2f95af9892b5bb8d0e21e
JHONATAN JANSSEN SEGURA GALINDO <jsegurag@ripley.com.pe>
2023-07-12 11:21:26 -0500

    add: get pokemon by name, no or id

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    bufferLogs: true,
+    snapshot: true,
     cors: { allowedHeaders: '*', origin: '*' },
   });
   app.setGlobalPrefix('api/v2');
--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -2,12 +2,14 @@ import {
   Injectable,
   BadRequestException,
   InternalServerErrorException,
+  NotFoundException,
 } from '@nestjs/common';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
-import { Model } from 'mongoose';
+import { Model, isValidObjectId } from 'mongoose';
 import { Pokemon } from './entities/pokemon.entity';
 import { InjectModel } from '@nestjs/mongoose';
+import { isNumber } from 'class-validator';
 
 @Injectable()
 export class PokemonService {
@@ -24,19 +26,46 @@ export class PokemonService {
       return pokemon;
     } catch (error) {
       if (error.code === 11000) {
-        throw new BadRequestException(`Pokemon exist in db. ${JSON.stringify(error.keyValue)} `);
+        throw new BadRequestException(
+          `Pokemon exist in db. ${JSON.stringify(error.keyValue)} `,
+        );
       }
       console.log(error);
       throw new InternalServerErrorException();
     }
   }
 
-  findAll() {
+  async findAll() {
     return `This action returns all pokemon`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} pokemon`;
+  async findOne(value: string) {
+    let pokemonFound = null;
+
+    if (isNumber(Number(value))) {
+      pokemonFound = await this.pokemonModel.findOne({
+        no: value,
+      });
+    }
+
+    if (!pokemonFound && isValidObjectId(value)) {
+      pokemonFound = await this.pokemonModel.findOne({
+        _id: value,
+      });
+    }
+
+    if (!pokemonFound) {
+      pokemonFound = await this.pokemonModel.findOne({
+        name: value,
+      });
+    }
+
+    console.log({ pokemonFound });
+    if (!pokemonFound) {
+      throw new NotFoundException(`Not found Pokemon with no ${value}`);
+    }
+
+    return pokemonFound;
   }
 
   update(id: number, updatePokemonDto: UpdatePokemonDto) {
